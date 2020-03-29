4337c63d90375238d1ee5a891eff03fb2e42636b
ScottTomassetti <scott_t_ct@hotmail.com>
2020-03-29 08:12:23 -0400

    Added CleanWebpackPlugin to the project and updated the webpack.config.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-<<<<<<< HEAD
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
  
 module.exports = {
   entry: './js/index.js',
@@ -26,40 +26,14 @@ module.exports = {
             loader: 'file-loader',
           },
         ],
-      }
+      },
+      
     ]
-  }
-}
-=======
-const path = require('path');
- 
-module.exports = {
-  entry: './js/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist/'
   },
-  mode: 'development',
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: [ 'style-loader', 'css-loader' ]
-      },
-      {
-        test: /\.scss$/i,
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-      },
-      {
-        test: /\.(gif|jpg|jpeg|png)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
-      }
-    ]
-  }
-}
->>>>>>> 5dccd5400f1fd7ad1ebeb0e59c5526bd68957ca7
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+};
+
+
+
